d1694df0e08afafb6fd5cffc9de682504bcece1b
Joe Casabona <jcasabona@gmail.com>
2015-12-31 15:21:23 -0500

    Nifty SVG swap support thing

--- resolutions/js/main.js
+++ resolutions/js/main.js
@@ -1,3 +1,10 @@
 $( "article li" ).click(function() {
   $( this ).toggleClass( "strike" );
+  var icon = 'icon-circle';
+  icon += ( ! $(this).hasClass('strike') ) ? '-thin' : '';
+
+  $('svg', this).attr('class', icon);
+  $('svg use', this ).attr('xlink:href', 'img/icons.svg#' + icon);
 });
+
+//$(".year-2016 article li").html('' + $(this).html);
